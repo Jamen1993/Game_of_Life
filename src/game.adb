98dd0348b3d75e0b229c64ee8b273f94abde1320
Simon Diehl <j.simon.diehl@googlemail.com>
2018-02-10 15:51:00 +0100

    Finished game logic

--- src/game.adb
+++ src/game.adb
@@ -1,5 +1,3 @@
-with Ada.Real_Time; use Ada.Real_Time;
-
 package body Game is
 
    protected body Protected_Game_Field_t is
@@ -25,40 +23,106 @@ package body Game is
 
    end Protected_Game_Field_t;
 
-   task body Game_Task is
-      TASK_PERIOD : constant Time_Span := Milliseconds (100);
-      next_time : Time := Clock;
-   begin
-      null;
+   protected body Game_Control_t is
+      procedure set_task_period (new_task_period : in Time_Span) is
+      begin
+         task_period := new_task_period;
+      end set_task_period;
 
-      next_time := next_time + TASK_PERIOD;
-      delay until next_time;
+      procedure set_autoprogression_active (new_state : in Boolean) is
+      begin
+         autoprogression_active := new_state;
+      end set_autoprogression_active;
+
+      procedure set_next_step_single is
+      begin
+         next_step_single := True;
+      end set_next_step_single;
+
+      function get_task_period return Time_Span is
+      begin
+         return task_period;
+      end get_task_period;
+
+      function is_autoprogression_active return Boolean is
+      begin
+         return autoprogression_active;
+      end is_autoprogression_active;
+
+      procedure is_next_step_single (result : out Boolean) is
+      begin
+         if next_step_single then
+            result := True;
+            next_step_single := False;
+         else
+            result := False;
+         end if;
+      end is_next_step_single;
+   end Game_Control_t;
+
+   task body Game_Task is
+      next_time : Time := Clock;
+
+      execute_step : Boolean;
+   begin
+      loop
+         game_control.is_next_step_single (execute_step);
+         execute_step := execute_step or game_control.is_autoprogression_active;
+
+         if execute_step then
+            determine_new_cell_states;
+         end if;
+
+         next_time := next_time + game_control.get_task_period;
+         delay until next_time;
+      end loop;
    end Game_Task;
 
    procedure determine_new_cell_states is
       new_game_field : Game_Field_t;
       living_neighbours : Living_Neighbours_t;
    begin
-      for x in new_game_field'Range(1) loop
-         for y in new_game_field'Range(2) loop
-            living_neighbours := count_living_neighbours (x, y);
+      for it_x in new_game_field'Range (1) loop
+         for it_y in new_game_field'Range (2) loop
+            living_neighbours := count_living_neighbours (it_x, it_y);
 
             if living_neighbours = 3 then
-               new_game_field (x, y) := alive;
+               new_game_field (it_x, it_y) := alive;
             elsif living_neighbours < 2 then
-               new_game_field (x, y) := dead;
+               new_game_field (it_x, it_y) := dead;
             elsif living_neighbours > 3 then
-               new_game_field (x, y) := dead;
+               new_game_field (it_x, it_y) := dead;
             else
-               new_game_field (x, y) := game_field.get_cell_state (x, y);
+               new_game_field (it_x, it_y) := game_field.get_cell_state (it_x, it_y);
             end if;
          end loop;
       end loop;
+
+      game_field.set_all (new_game_field);
    end determine_new_cell_states;
 
    function count_living_neighbours (x, y : in Game_Field_Index_t) return Living_Neighbours_t is
+      living_neighbours : Living_Neighbours_t := 0;
+
+      x_to_test, y_to_test : Positive;
    begin
-      return 0;
+      for x_deviation in Integer range -1 .. 1 loop
+         for y_deviation in Integer range -1 .. 1 loop
+            --  Skip the targeted cell itself
+            if not (x_deviation = 0 and y_deviation = 0) then
+               x_to_test := (x + x_deviation + Game_Field_Index_t'Last) mod Game_Field_Index_t'Last;
+               y_to_test := (y + y_deviation + Game_Field_Index_t'Last) mod Game_Field_Index_t'Last;
+
+               case game_field.get_cell_state (x_to_test, y_to_test) is
+                  when alive => living_neighbours := living_neighbours + 1;
+                  when dead => null;
+               end case;
+            end if;
+         end loop;
+      end loop;
+
+      return living_neighbours;
+
    end count_living_neighbours;
 
 end Game;
--- src/game.ads
+++ src/game.ads
@@ -1,7 +1,9 @@
+with Ada.Real_Time; use Ada.Real_Time;
+
 package Game is
 
    type Cell_State_t is (dead, alive);
-   type Game_Field_Index_t is new Positive range 1 .. 100;
+   subtype Game_Field_Index_t is Positive range 1 .. 100;
    type Game_Field_t is array (Game_Field_Index_t, Game_Field_Index_t) of Cell_State_t;
 
    type Living_Neighbours_t is new Natural range 0 .. 8;
@@ -16,7 +18,23 @@ package Game is
       game_field : Game_Field_t := (others => (others => dead));
    end Protected_Game_Field_t;
 
+   protected type Game_Control_t is
+      procedure set_task_period (new_task_period : in Time_Span);
+      procedure set_autoprogression_active (new_state : in Boolean);
+      procedure set_next_step_single;
+      procedure is_next_step_single (result : out Boolean);
+
+      function get_task_period return Time_Span;
+      function is_autoprogression_active return Boolean;
+   private
+      task_period : Time_Span := Milliseconds (100);
+
+      autoprogression_active : Boolean := False;
+      next_step_single : Boolean := False;
+   end Game_Control_t;
+
    game_field : Protected_Game_Field_t;
+   game_control : Game_Control_t;
 
 private
    procedure determine_new_cell_states;
